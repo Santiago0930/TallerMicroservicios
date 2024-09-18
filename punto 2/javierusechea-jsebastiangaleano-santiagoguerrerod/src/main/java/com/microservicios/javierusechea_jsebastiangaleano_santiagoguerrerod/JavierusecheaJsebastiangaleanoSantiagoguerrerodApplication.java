eba9dcca57ba66ac8ff996c0ee8ae681a094b61f
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-09-18 16:34:17 -0500

    FEAT: Punto 2 Full

---
+++ punto 2/javierusechea-jsebastiangaleano-santiagoguerrerod/src/main/java/com/microservicios/javierusechea_jsebastiangaleano_santiagoguerrerod/JavierusecheaJsebastiangaleanoSantiagoguerrerodApplication.java
@@ -0,0 +1,14 @@
+package com.microservicios.javierusechea_jsebastiangaleano_santiagoguerrerod;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+@SpringBootApplication
+@EnableDiscoveryClient
+public class JavierusecheaJsebastiangaleanoSantiagoguerrerodApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(JavierusecheaJsebastiangaleanoSantiagoguerrerodApplication.class, args);
+	}
+
+}
