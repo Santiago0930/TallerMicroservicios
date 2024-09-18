d0bca2c5c1e02c7627cb2763178f028e8071be02
el-sebas-galeano <jsebastian-galeano@javeriana.edu.co>
2024-09-18 16:57:18 -0500

    FEAT: Punto 4 Full

---
+++ punto 4/javierusechea-jsebastiangaleano-santiagoguerrerod/src/test/java/com/microservicios/javierusechea_jsebastiangaleano_santiagoguerrerod/JavierusecheaJsebastiangaleanoSantiagoguerrerodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.microservicios.javierusechea_jsebastiangaleano_santiagoguerrerod;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavierusecheaJsebastiangaleanoSantiagoguerrerodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
