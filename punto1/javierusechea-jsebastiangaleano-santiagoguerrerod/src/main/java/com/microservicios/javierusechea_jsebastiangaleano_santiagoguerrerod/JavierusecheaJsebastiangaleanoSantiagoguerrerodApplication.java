2f881622eaaa91fde095b1ef5db42e5eb1cb7713
KaaD <jemirouseche@hotmail.com>
2024-09-25 02:24:59 -0500

    Correcciones y todo funcional

--- punto1/javierusechea-jsebastiangaleano-santiagoguerrerod/src/main/java/com/microservicios/javierusechea_jsebastiangaleano_santiagoguerrerod/JavierusecheaJsebastiangaleanoSantiagoguerrerodApplication.java
+++ punto1/javierusechea-jsebastiangaleano-santiagoguerrerod/src/main/java/com/microservicios/javierusechea_jsebastiangaleano_santiagoguerrerod/JavierusecheaJsebastiangaleanoSantiagoguerrerodApplication.java
@@ -2,8 +2,11 @@ package com.microservicios.javierusechea_jsebastiangaleano_santiagoguerrerod;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
+
 public class JavierusecheaJsebastiangaleanoSantiagoguerrerodApplication {
 
 	public static void main(String[] args) {
